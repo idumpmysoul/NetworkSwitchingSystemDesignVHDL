637a6483a0da8761b3877c4210b9b88c0c0736e8
idumpmysoul <abedzebua777@gmail.com>
2024-12-05 06:04:42 +0700

    Initial Commit

---
+++ switch.vhd
@@ -0,0 +1,18 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.numeric_std.all;
+
+entity switch is
+    port (
+        INPUT   :   in std_logic_vector(7 downto 0);
+
+    );
+end entity switch;
+
+architecture rtl of switch is
+    
+begin
+    
+    
+    
+end architecture rtl;
